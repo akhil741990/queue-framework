ab790d712ac9da69b474ccc8b5e8a080d6f9a90b
Akhil Vishwanathan Pillai <akhil741990@gmail.com>
2018-12-18 00:06:27 +0530

    Added unboundedQ

--- src/queuemanger/queues/UnBoundedQ.java
+++ src/queuemanger/queues/UnBoundedQ.java
@@ -2,51 +2,60 @@ package queuemanger.queues;
 
 import java.util.concurrent.LinkedBlockingQueue;
 
+import queue.framework.exception.ApplicationRejected;
+import queue.framework.exception.QueueNotFound;
 import queue.framework.exception.TaskNotQueued;
 import queuemanger.core.Task;
 import user.User;
 
 public class UnBoundedQ<T extends Task> extends AbstractQ<T>{
 
-	LinkedBlockingQueue<T> queue;
+	private LinkedBlockingQueue<T> queue;
+	private int dispatchIndex;
+	private long expiry;  // TODO : Add TimeUnit
 	public UnBoundedQ(String name) {
 		super(name);
 		this.queue = new LinkedBlockingQueue<>();
 	}
 	@Override
-	public void subscribe(User<T> user) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public void add(T task) throws TaskNotQueued {
-		// TODO Auto-generated method stub
+		task.setTimestamp(System.currentTimeMillis());
+		task.setCurrentQ(this.getName());
+		try {
+			queue.put(task);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			throw new TaskNotQueued(e.getMessage());
+		}
 		
 	}
-
 	@Override
 	public void setExpiryInMs(long expiry) {
-		// TODO Auto-generated method stub
+		this.expiry = expiry;
 		
 	}
-
 	@Override
 	public void dispatchTask(T task) {
-		// TODO Auto-generated method stub
+		
+		int nextdispatchIndex =  dispatchIndex++ % subscribers.size();
+		User<T> user = subscribers.get(nextdispatchIndex); 
+		System.out.println("Dispacthed to user:"+user.getName());
+ 		try {
+			user.process(task);
+		} catch (QueueNotFound | TaskNotQueued e) {
+			// TODO handle error processing and add it back to the relevantQ
+			e.printStackTrace();
+		} catch (ApplicationRejected e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
-
-	@Override
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 	@Override
 	public T deQ() throws InterruptedException {
 		// TODO Auto-generated method stub
-		return null;
+		return queue.take();
 	}
+	
 
 }
